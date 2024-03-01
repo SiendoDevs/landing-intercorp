268679c972f9f1d1ba384e4bcc4709507c2215a9
SiendoDevs <desarrollo@siendostudio.com>
2024-03-01 20:46:18 -0300

    Log in

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -44,7 +44,7 @@ export default function Example() {
             ))}
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            <a href="https://www.intercorpmarketingawards.com/" className="text-sm font-semibold leading-6 text-gray-900">
               Log in <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
